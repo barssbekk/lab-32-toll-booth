50128e7006f580a3596107d282081ac46b6ce4ac
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-24 12:10:25 -0700

    add deq

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 // COMSC-210 | Lab 32 | Barsbek
 #include <iostream>
+#include <deque>
 #include "Car.h"
 
 using namespace std;
 
+const int NUM_CAR = 2;
+
 int main() {
+    deque<Car> cars;
+
+    cout << "Initial queue:\n";
+
+
     return 0;
 }
