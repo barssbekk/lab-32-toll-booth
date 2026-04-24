776546d74c3f4f7ad56a4bd33aa7d37ca9402f78
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-24 12:29:22 -0700

    upd deq

--- main.cpp
+++ main.cpp
@@ -6,13 +6,26 @@
 using namespace std;
 
 const int NUM_CAR = 2;
+const int PROB_PRCNT = 100;
 
 int main() {
+    srand(time(nullptr));
     deque<Car> line;
-    for (int i = 0; i < NUM_CAR; ++i) // Initial val
+    for (int i = 0; i < NUM_CAR; ++i) { // Initial val
+        Car car;
+        line.push_back(car);
+    }
 
     cout << "Initial queue:\n";
+    for (auto& cars : line)
+        cars.print();
 
+
+    int numOperation = 1;
+    do {
+        cout << "Time: " << numOperation << " Operation: ";
+        int probability = rand() % 100;
+    } while (!line.empty());
 
     return 0;
 }
