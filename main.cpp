85f22fb09c16ba0d6163940c7166e01ab7e1cb38
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-26 21:54:22 -0700

    Milestone 5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// COMSC-210 | Lab 32 | Barsbek
+// COMSC-210 | Lab 33 | Barsbek
 #include <iostream>
 #include <deque>
 #include <cstdlib>
@@ -11,62 +11,84 @@ using namespace std;
 const int NUM_CAR = 2;              // initial num of cars
 const int PROB_PRCNT = 100;         // probability range
 const int LEAVE_CHANCE = 46;        // chance car leaves
-const int JOIN_CHANCE = 39;
-const int SHIFT_LANE_CHANCE = 15;
+const int JOIN_CHANCE = 39;         //
 const int MAX_TOLL_BOOTH = 4;       // max amount of toll both lan
-const int MAX_TIME_PERIOD = 20;
+const int MAX_TIME_PERIOD = 20;     // 
 
 void printLanes(const array<deque<Car>, MAX_TOLL_BOOTH>& lanes);
-// void shiftLanes(deque<Car> carShift, array<deque<Car>, MAX_TOLL_BOOTH> lanes);
 
 int main() {
     srand(time(nullptr));
 
     array<deque<Car>, MAX_TOLL_BOOTH> lanes;
-    for (int i = 0; i < MAX_TOLL_BOOTH; ++i) {
+    for (int i = 0; i < MAX_TOLL_BOOTH; ++i) { // fill each lane w 2 cars
         for (int j = 0; j < NUM_CAR; j++) {
             lanes.at(i).push_back(Car());
         }
     }
 
+    // init val
     printLanes(lanes);
+    cout << '\n';
 
     int numOperation = 1;
     do {
         cout << "\nTime: " << numOperation << '\n';
+
         for (int i = 0; i < MAX_TOLL_BOOTH; ++i) {
             cout << "Lane " << i + 1 << " ";
-
-            if (rand() % 2 == 0) {  // 50/50 leave
-                if (!lanes.at(i).empty()) {
-                    cout << "Paid: ";
-                    lanes.at(i).front().print();
-                    lanes.at(i).pop_front();
+            int rnd = rand() % PROB_PRCNT;
+            if (lanes.at(i).empty()) {
+                if (rand() % 2 == 0) { // 50/50
+                    cout << "Joined: ";
+                    Car car;
+                    lanes.at(i).push_back(car);
+                    car.print();
                 } else {
                     cout << "Empty\n";
                 }
-            } else {                // join
-                cout << "Joined: ";
-                Car joinedCar;
-                lanes.at(i).push_back(joinedCar);
-                joinedCar.print();
             }
-        }
-        for (int i = 0; i < MAX_TOLL_BOOTH; ++i) {
-            cout << "Lane " << i + 1 << " Queue:\n";
-            for (auto& car : lanes.at(i)) {
+            else if (rnd < LEAVE_CHANCE) { // car leaves
+                cout << "Paid: ";
+                lanes.at(i).front().print();
+                lanes.at(i).pop_front();
+            }
+            else if (rnd < LEAVE_CHANCE + JOIN_CHANCE) { // car joins
+                cout << "Joined: ";
+                Car car;
+                lanes.at(i).push_back(car);
                 car.print();
             }
+            else { // switch lane
+                Car movingCar = lanes.at(i).back();
+                lanes.at(i).pop_back();
+
+                int newLane;
+                do {
+                    newLane = rand() % MAX_TOLL_BOOTH;
+                } while (newLane == i);
+
+                lanes.at(newLane).push_back(movingCar);
+
+                cout << "Switched: ";
+                movingCar.print();
+            }
         }
+
+        printLanes(lanes);
+
         ++numOperation;
+
     } while (numOperation <= MAX_TIME_PERIOD);
     return 0;
 }
 
+// print each lane
 void printLanes(const array<deque<Car>, MAX_TOLL_BOOTH>& lanes) {
     for (int i = 0; i < MAX_TOLL_BOOTH; ++i) {
         cout << "Lane " << i + 1 << " Queue:\n";
         for (const auto& car : lanes.at(i)) {
+            cout << '\t';
             car.print();
         }
     }
